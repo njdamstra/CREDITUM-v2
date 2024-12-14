f74aae66bdfbea519aa50f88f28a38a807b87f46
100531302 <njdamstra@berkeley.edu>
2024-12-13 19:27:26 -0800

    deleted bad front end file

--- frontend/src/utils/config.js
+++
@@ -1,3 +0,0 @@
-export const NETWORK = "localhost"; // Change to "sepolia" or other networks for testnet/mainnet
-export const CHAIN_ID = 1337; // Update based on the network you're using
-export const DEFAULT_GAS = 3000000; // Gas limit for transactions
